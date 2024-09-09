1f36fd76192062da4e47fe520edd5d66e5934f0c
Peterwens Dourolph Denival <109047885+Peterwens@users.noreply.github.com>
2024-09-09 15:22:06 -0400

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
 
     # Path for logout
     path('logout', views.logout_request, name='logout'),
-
+    path(route='get_cars', view=views.get_cars, name ='getcars'),
     # Placeholder for dealer reviews view (uncomment and implement the view logic)
     # path('dealer-reviews/<int:dealer_id>/', views.get_dealer_reviews, name='dealer_reviews'),
 
