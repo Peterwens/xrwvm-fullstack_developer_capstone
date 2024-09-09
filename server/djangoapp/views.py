e27325c37881e6a783cdf7f31ed3c47e7e4aba69
Peterwens Dourolph Denival <109047885+Peterwens@users.noreply.github.com>
2024-09-09 15:21:17 -0400

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -7,9 +7,22 @@ from django.contrib import messages
 import logging
 import json
 from datetime import datetime
+from .models import CarMake, CarModel
 
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
+
+#get car
+def get_cars(request):
+    count = CarMake.objects.filter().count()
+    print(count)
+    if(count == 0):
+        initiate()
+    car_models = CarModel.objects.select_related('car_make')
+    cars = []
+    for car_model in car_models:
+        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
+    return JsonResponse({"CarModels":cars})
 
 # Create a `login_request` view to handle sign-in requests
 @csrf_exempt  # Only use @csrf_exempt if absolutely necessary (e.g., in development)
